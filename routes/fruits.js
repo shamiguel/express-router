dbfb0563eadc6f38e2d4eb9f5d023be7a8550f16
Shami Tomita <shamito@google.com>
2023-10-19 15:06:20 -0400

    MORE VALIDATIONS

--- routes/fruits.js
+++ routes/fruits.js
@@ -17,7 +17,8 @@ router.get('/:id', async(req, res)=>{
 })
 
 router.post('/',[
-    check("color").not().isEmpty().trim()
+    check("color").not().isEmpty().trim().optional(),
+    check("name").not().isEmpty().trim().isLength({min: 5, max: 20}).optional(),
 ], async(req, res)=>{
     console.log("creating...")
     const errors = validationResult(req)
@@ -31,14 +32,23 @@ router.post('/',[
     }
 })
 
-router.put('/:id', async(req, res)=>{
-    const id = req.params.id;
-    //console.log(req.body)
-    const fruit = await Fruit.findByPk(id);
-    const data = req.body;
-    //console.log(data)
-    const updatedFruit = await fruit.update(data);
-    res.json(updatedFruit);
+router.put('/:id',[
+    check("color").not().isEmpty().trim().optional(),
+    check("name").not().isEmpty().trim().isLength({min: 5, max: 20}).optional(),
+], async(req, res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.json({error: errors.array()})
+    }else{
+        const id = req.params.id;
+        //console.log(req.body)
+        const fruit = await Fruit.findByPk(id);
+        const data = req.body;
+        //console.log(data)
+        const updatedFruit = await fruit.update(data);
+        res.json(updatedFruit);
+    }
 })
 
 router.delete('/:id', async(req, res)=>{
--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,8 @@ router.get('/:id', async(req, res)=>{
 })
 
 router.post('/',[
-    check("name").not().isEmpty().trim()
+    check("name").not().isEmpty().trim().isLength({min: 5, max: 15}),
+    check("age").not().isEmpty().trim()
 ], async(req, res)=>{
     const error = validationResult(req)
     if(!error.isEmpty()){
@@ -28,14 +29,22 @@ router.post('/',[
     }
 });
 
-router.put('/:id', async(req, res)=>{
-    const id = req.params.id;
-    //console.log(req.body)
-    const user = await User.findByPk(id);
-    const data = req.body;
-    //console.log(data)
-    const updatedUser = await user.update(data);
-    res.json(updatedUser);
+router.put('/:id',[
+    check("name").not().isEmpty().trim().isLength({min: 5, max: 15}).optional(),
+    check("age").not().isEmpty().trim().optional()
+], async(req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.json({error: errors.array()})
+    }else{
+        const id = req.params.id;
+        //console.log(req.body)
+        const user = await User.findByPk(id);
+        const data = req.body;
+        //console.log(data)
+        const updatedUser = await user.update(data);
+        res.json(updatedUser);
+    }
 })
 
 router.delete('/:id', async(req, res)=>{
